c958efa6dbc0252a0340b73e8bcfce523b19c19c
root <root@caronaVM.qwfunwwfkdeunajjy21omnmfjd.bx.internal.cloudapp.net>
2020-04-14 04:20:07 +0000

    two projects are added

---
+++ globalproject/src/main/java/globallogic/App.java
@@ -0,0 +1,13 @@
+package globallogic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
